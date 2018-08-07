1240a6a4366336a211a2dd99d6abcff1f6304b40
Gigi Wolff <gigi.wolff@gmail.com>
2018-08-07 18:44:20 -0400

    ready to send to heroku

--- app/models/search_all_allergens.rb
+++ app/models/search_all_allergens.rb
@@ -3,7 +3,7 @@ def search_all_allergens(search_item, user_id)
   # use ILIKE in production and LIKE in development
   allergens = Allergen.where "user_id = ?", user_id
 
-  #return allergens.where "category ILIKE ? OR substances ILIKE ?", "%#{search_item}%", "%#{search_item}%"
-  return allergens.where "category LIKE ? OR substances LIKE ?", "%#{search_item}%", "%#{search_item}%"
+  return allergens.where "category ILIKE ? OR substances ILIKE ?", "%#{search_item}%", "%#{search_item}%"
+  #return allergens.where "category LIKE ? OR substances LIKE ?", "%#{search_item}%", "%#{search_item}%"
 
 end
