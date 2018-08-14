d2a1638047b39bf734d44153e8ec32a356753fc2
Gigi Wolff <gigi.wolff@gmail.com>
2018-08-14 18:31:12 -0400

    cosmetic changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -58,9 +58,9 @@ class Product < ApplicationRecord
       # check each ingredient in allergen db by looking for it in the substances 
       # associated with each allergen category
       # (production:)
-      allergens_causing_reactions = Allergen.where "user_id = ? AND substances ILIKE ?", self.user_id, "%#{ingredient}%" #production
+      #allergens_causing_reactions = Allergen.where "user_id = ? AND substances ILIKE ?", self.user_id, "%#{ingredient}%" #production
       # (development:)
-      #allergens_causing_reactions = Allergen.where "user_id = ? AND substances LIKE ?", self.user_id, "%#{ingredient}%" #development
+      allergens_causing_reactions = Allergen.where "user_id = ? AND substances LIKE ?", self.user_id, "%#{ingredient}%" #development
       # if ingredient matches allergens in db
       unless allergens_causing_reactions.empty? 
         # for each allergen that contains allergic substances
