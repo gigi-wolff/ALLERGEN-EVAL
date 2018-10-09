8f0dc9e0e7d83d60ace51ffbbb35b3efa68e5d9d
Gigi Wolff <gigi.wolff@gmail.com>
2018-10-09 14:27:22 -0400

    address and phone cannot be blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :address, presence: true
+  validates :phone, presence: true
 
 end
